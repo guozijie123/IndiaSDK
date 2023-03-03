5e6dcbb371f068a8a866d56a8ae6f7c1abf341d9
Douglas <924382582@qq.com>
2023-03-03 11:11:54 +0800

    dw

--- demo/demo/ViewController.m
+++ demo/demo/ViewController.m
@@ -16,7 +16,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"begin");
+    NSLog(@"begin~~~~~~~~");
 }
 
 - (IBAction)fblogin:(id)sender {
